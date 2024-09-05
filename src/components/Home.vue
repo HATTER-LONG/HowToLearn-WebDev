319895619f6cb7c0baa11f8c43a1dafc05cc71cb
Layton <caolei6767@gmail.com>
2024-09-06 07:08:48 +0800

    [VUE 7]refactor: Update Home.vue to use Element-Plus components for calculator functionality

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted, watch } from "vue"
 import { usePageStore } from "@/stores/pageStore"
-
+import { Check, Delete, Edit, Message, Search, Star } from "@element-plus/icons-vue"
 const pageStore = usePageStore()
 const num1 = ref(pageStore.homeInputs.num1)
 const num2 = ref(pageStore.homeInputs.num2)
@@ -22,10 +22,38 @@ onMounted(() => {
 })
 </script>
 <template>
-  <div class="calculator">
-    <input type="number" v-model="num1" placeholder="输入第一个数" />
-    <input type="number" v-model="num2" placeholder="输入第二个数" />
-    <button @click="calculate()">计算加法</button>
-    <div class="result" id="result"></div>
+  <div class="body">
+    <div class="calculator">
+      <el-input-number type="number" v-model="num1" placeholder="输入第一个数" />
+      <el-text type="primary">+</el-text>
+      <el-input-number type="number" v-model="num2" placeholder="输入第二个数" />
+      <el-button @click="calculate">计算加法</el-button>
+    </div>
+    <div class="result-container"><el-text class="result" id="result"></el-text></div>
   </div>
 </template>
+
+<style scoped>
+.calculator {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.el-input-number {
+  width: 200px;
+  margin: 0 10px; /* 左右各增加10px的间隔 */
+}
+.el-text {
+  font-size: 30px;
+  align-items: center;
+}
+.result-container {
+  width: 100%;
+  margin-top: 10px; /* 增加一些上间距 */
+}
+.result {
+  display: block;
+  width: 100%;
+  text-align: center; /* 居中显示结果 */
+}
+</style>
